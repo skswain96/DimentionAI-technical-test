61ae3b2230c314cce4c87bed216a8cef1dd5d44b
skswain_pwip <sudhanshu@pwip.co>
2024-11-01 01:51:30 +0530

    updated package.json script build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,4 @@
 // prisma.ts
-
-// import { PrismaClient } from "@prisma/client";
-
-// const prisma = new PrismaClient();
-
-// export default prisma;
-
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
