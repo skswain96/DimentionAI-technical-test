9e88bae96c4100b43670f11c3bd512462d65e0dc
skswain_pwip <sudhanshu@pwip.co>
2024-11-01 01:42:44 +0530

    updated prisma.ts file to support single instance

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,23 @@
 // prisma.ts
 
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
